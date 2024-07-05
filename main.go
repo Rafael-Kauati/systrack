d20e2e3eba50ac7aabe62f2ca4e91a0f2084fad4
tk <kauati.toiti@gmail.com>
2024-07-05 16:42:17 +0100

    flag options to set the port number

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
     "systrack/handlers"
 )
 
 func main() {
+    // Define a flag for the port with a default value of ":8000"
+    port := flag.String("port", ":8000", "Specify the port to run the server on")
+    
+    // Parse the command-line flags
+    flag.Parse()
+    
     http.HandleFunc("/api/resources", handlers.GetResources)
-    log.Println("Server is running on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    
+    // Log the message with the specified or default port
+    log.Printf("Server is running and listening on port %s\n", *port)
+    log.Fatal(http.ListenAndServe(*port, nil))
 }
